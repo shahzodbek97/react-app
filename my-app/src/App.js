6ca961771f16fc280351c067aca95dc205fb32bf
shahzodbek97 <xamdamboyev97@mail.ru>
2021-10-24 14:39:21 +0500

    react tut

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import react from "react"
+import Header from "./components/Header";
+
+function App (){
+    return (
+        <div>
+            <Header />
+        </div>
+    );
+}
+
+export default App;
---
+++ my-app/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Paper from '@mui/material/Paper';
+
+
+function Header() {
+    return (
+        <Paper>
+            <h1>Header</h1>
+        </Paper>
+    );
+}
+
+export default Header;
